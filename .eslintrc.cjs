019a6867a9dc05cd10e2454fb9fe768138f26bbe
Oleh <olegbrony@gmail.com>
2024-03-06 14:24:31 +0200

    Revert "Feature: initial setup (#1)"
    This reverts commit 2c3d834142d9b597571e4104401554186f04d2f4.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,13 +5,10 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
-    'plugin:jsx-a11y/recommended',
-    'prettier'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh', 'jsx-a11y', 'prettier'],
+  plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
--- src/app/index.tsx
+++
@@ -1,5 +0,0 @@
-import "./styles/index.css";
-
-export function App() {
-  return <h1>Brello</h1>;
-}
